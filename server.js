bceeab6ea93e6acf2070b6d8c485cc658bce828d
Weliton Moreira <SEU_EMAIL_DO_GITHUB>
2026-07-02 18:33:27 -0300

    Primeira versão FXBOT

---
+++ server.js
@@ -0,0 +1,24 @@
+
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+	res.json({
+		status: "online",
+		projeto: "FXBOT Trader",
+		versao: "1.0"
+	});
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+	console.log(`Servidor iniciado na porta ${PORT}`);
+});
